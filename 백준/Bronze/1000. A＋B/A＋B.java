293f26b9150bc4fb8a026e874746dd5ed9dc4098
J93es <j93es.jung@gmail.com>
2024-06-08 12:55:40 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int     num1 = 0;
+        int     num2 = 0;
+        Scanner scanTool = new Scanner(System.in);
+        
+        num1 = scanTool.nextInt();
+        num2 = scanTool.nextInt();
+        System.out.println(num1 + num2);
+    }
+    
+}
