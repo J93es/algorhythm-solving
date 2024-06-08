c168336b1ed6e52dec35246d1fd96fb92344de00
J93es <j93es.jung@gmail.com>
2024-06-08 12:40:50 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5020 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define min(a, b) (a < b ? a : b)
+
+int dp[1000001] = { 0, };
+
+int main(void) {
+        int n;
+        
+        scanf("%d", &n);
+        
+        if (n == 1) {
+                printf("0\n");
+                return 0;
+        }
+
+        if (n <= 3) {
+                printf("1\n");
+                return 0;
+        }
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= n; i++)
+        {
+                dp[i] = dp[i - 1] + 1;
+                if (i % 3 == 0)
+                        dp[i] = min(dp[i], dp[i / 3] + 1);
+                if (i % 2 == 0)
+                        dp[i] = min(dp[i / 2] + 1, dp[i]);
+        }
+
+        printf("%d\n", dp[n]);
+        
+        return 0;
+}
