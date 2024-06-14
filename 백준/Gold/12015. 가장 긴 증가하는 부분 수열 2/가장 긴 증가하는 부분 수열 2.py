ce00a50fd1d159cc9922a86b30472dbd4dbbada4
J93es <j93es.jung@gmail.com>
2024-06-14 10:20:48 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 3688 ms, Memory: 155556 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,22 @@
+input()
+
+cases = map(int, input().split())
+
+memorization = [0]
+
+for case in cases:
+	if memorization[-1] < case:
+		memorization.append(case)
+	else:
+		left = 0
+		right = len(memorization)
+		while left<right:
+			mid = (left+right)//2
+            
+			if memorization[mid] < case:
+				left = mid + 1
+			else:
+				right = mid
+		memorization[right] = case
+
+print(len(memorization) - 1)
