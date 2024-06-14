e1c66ecf2254be070e6b9212544abd7bd5221110
J93es <j93es.jung@gmail.com>
2024-06-14 10:32:18 +0900

    [Silver III] Title: N과 M (3), Time: 476 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int N, M;
+
+
+void BackTracking(int arr[], int pos, int cur)
+{
+        int i = 0;
+        if (cur > N || cur < 1)
+                return;
+        arr[pos] = cur;
+        i = 0;
+        if (pos == M - 1)
+        {
+                while (i < M)
+                        printf("%d ", arr[i++]);
+                printf("\n");
+                return;
+        }
+        i = 1;
+        while (i < N+1)
+                BackTracking(arr, pos+1, i++);
+        return;
+}
+
+
+int main(void)
+{
+        int i = 1;
+        int arr[9] = { 0, };
+        scanf("%d %d", &N, &M);
+        while (i < N+1)
+                BackTracking(arr, 0, i++);
+}
